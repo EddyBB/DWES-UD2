c29c5f81359a936fe6c541f5f201589812dbd508
Eddy <eddy.gonzalez.chavez.al@iespoligonosur.org>
2021-09-29 12:35:35 +0200

    acabar ejer 9

---
+++ ejercicio9.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio1</title>
+</head>
+<body>
+    <?php
+        print "<p>\$_SERVER[PHP_SELF]: $_SERVER[PHP_SELF]</p>\n";
+        //https://www.mclibre.org/consultar/php/lecciones/php-variables-predefinidas.html
+    ?>
+</body>
+</html>
